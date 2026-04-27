4dd1e73ca2a5a22d096e94a3fff4a0f494ac4867
Jiklyp000f <150440963+Jiklyp000f@users.noreply.github.com>
2026-04-27 13:50:29 +0300

    Исправил картинку в карточках(просто обрезал исходник, теперь нужны только квадратные картинки)

--- src/widgets/Section/Section.tsx
+++ src/widgets/Section/Section.tsx
@@ -1,6 +1,6 @@
 import { Title } from "@/shared/ui/titles";
 import styles from "./styles.module.scss";
-import type { Variant } from "@/shared/ui/Titles/types";
+import type { Variant } from "@/shared/ui/titles/types";
 
 type Props = {
   title?: string;
